5f4130d1d7ea946b6edb2a2c1138a70012e1263b
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-04-09 00:52:14 +0200

    Render example story list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { QueryRenderer, graphql } from 'react-relay';
+import environment from '../../common/relayEnvironment';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello world
-      </div>
-    );
+const query = graphql`
+  query AppQuery {
+    stories {
+      id
+      title
+    }
   }
-}
+`;
+
+const App = () => {
+  return (
+    <QueryRenderer
+      environment={ environment }
+      query={ query }
+      variables={ {} }
+      render={ ( { error, props } ) => {
+        if ( error ) {
+          console.error( error );
+          return <pre>{ JSON.stringify( error, null, 2 ) }</pre>;
+        }
+
+        if ( props ) {
+          return (
+            <ul>
+              { props.stories.map( story => (
+                <li key={ story.id }>
+                  { story.title }
+                </li>
+              ) ) }
+            </ul>
+          );
+        }
+
+        return <div>loading...</div>;
+      } }
+    />
+  );
+};
 
 export { App };
