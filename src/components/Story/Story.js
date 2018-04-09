9cb85eda0cdc7ba6976ceb63001ca744ec48587f
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-04-09 23:21:25 +0200

    Set local state

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { createFragmentContainer, graphql } from 'react-relay';
+import {
+  createFragmentContainer,
+  graphql,
+  commitLocalUpdate,
+} from 'react-relay';
+import environment from '../../common/relayEnvironment';
 
-const _Story = ( props ) => (
-  <div>
-    <span>{ props.story.read ? 'read' : 'unread' } </span>
-    <span>{ props.story.title }</span>
-  </div>
-);
+const updateStoryReadState = ( id, read ) => {
+  commitLocalUpdate( environment, store => {
+    const record = store.get( id );
+    record.setValue( !read, 'read');
+  } );
+};
+
+const _Story = ( props ) => {
+  return (
+    <label>
+      <input
+        type='checkbox'
+        checked={ props.story.read || false }
+        onChange={ () => updateStoryReadState( props.story.id, props.story.read ) }
+      />
+      &nbsp;
+      <span>{ props.story.title }</span>
+    </label>
+  );
+};
 
 const Story = createFragmentContainer(
   _Story,
   graphql`
     fragment Story_story on Story {
+      id
       title
       read
     }
