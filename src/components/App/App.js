84e3a610c4563271e236b24f42fefb0da3cce7e0
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-04-11 00:20:47 +0200

    Render full story

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,7 +23,6 @@ class App extends React.PureComponent {
   }
 
   selectStory = ( selectedStory ) => {
-    console.log( selectedStory );
     this.setState( { selectedStory } );
   }
 
--- src/components/StoryView/StoryView.js
+++ src/components/StoryView/StoryView.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { QueryRenderer, graphql } from 'react-relay';
+import environment from 'common/relayEnvironment';
 
-const StoryView = ( props ) => {
-  return (
-    <div>view { props.storyId }</div>
-  );
-};
+const query = graphql`
+  query StoryViewQuery( $id: ID! ) {
+    story( id: $id ) {
+      id
+      title
+      content
+    }
+  }
+`;
+
+class StoryView extends React.PureComponent {
+  renderStory = ( storyProps ) => {
+    const { story } = storyProps;
+    return (
+      <article>
+        <pre>
+          { story.content }
+        </pre>
+      </article>
+    );
+  }
+
+  render() {
+    if ( !this.props.storyId ) {
+      return null;
+    }
+
+    return (
+      <div>
+        <QueryRenderer
+          environment={ environment }
+          query={ query }
+          variables={ {
+            id: this.props.storyId,
+          } }
+          render={ ( { error, props } ) => {
+            if ( error ) {
+              console.error( error );
+              return <pre>{ JSON.stringify( error, null, 2 ) }</pre>;
+            }
+
+            if ( props ) {
+              return this.renderStory( props );
+            }
+
+            return <div>loading...</div>;
+          } }
+        />
+      </div>
+    );
+  }
+}
 
 export { StoryView };
